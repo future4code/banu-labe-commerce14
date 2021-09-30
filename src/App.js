aa6f664dd1f80a14bc8b270898f632204edefea6
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-09-30 19:47:33 -0300

    Recoloquei todos os produtos no App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import './App.css';
-import Produtos from './Componentes/Cards/Cards';
 
-function App() {
+
+class Commerce extends React.Component {
+  state = [
+    {
+        id: 1,
+        nome: "Satélites Antigos",
+        value: 1000.00,
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Vanguard_1.jpg/300px-Vanguard_1.jpg"
+    },
+    {
+        id: 2,
+        nome: "Roupas Espaciais",
+        value: 800.00,
+        img: "https://preview.free3d.com/img/2015/08/2374217469742023762/xhezeoy5-900.jpg"
+    },
+    {
+        id: 3,
+        nome: "Camisetas com estampas divertidas relacionadas à espaço",
+        value: 150.00,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR46OfkN_dY4s8iJk4ht-vVe5Olsxn0a2JQmQ&usqp=CAU"
+    },
+    {
+        id: 4,
+        nome: "Viagens espaciais",
+        value: 1000000.00,
+        img: "https://s2.glbimg.com/zDASRlIxJ-7d8vPvZHLAT_aLX0A=/e.glbimg.com/og/ed/f/original/2019/05/23/mars2020-sendname-home.jpg"
+    },
+    {
+        id: 5,
+        nome: "Meteoritos",
+        value: 80000.00,
+        img: "http://igeologico.com.br/wp-content/uploads/2018/10/meteoritos_ferrosos-300x225.jpg"
+    },
+    {
+        id: 6,
+        nome: "Brinquedos e produtos infantis temáticos",
+        value: 80000.00,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgomTPHASDINgeySMKjDHJCjAnvOXjw0m4Yw&usqp=CAU"
+    },
+    {
+        id: 7,
+        nome: "Naves / Foguetes / Ônibus espaciais",
+        value: 80000.00,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9FODTCSvaRcnIEQJ3v5dx95KFJNelebs-1A&usqp=CAU"
+    }    
+]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <Produtos/>
-        
-      </header>
-    </div>
+   
   );
 }
 
-export default App;
+export default Commerce;
