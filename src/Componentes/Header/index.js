7b3e926ba06e1af42559a610bc92c5303443e933
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-10-01 19:57:55 -0300

    Criado itens da loja e botão Adicionar ao Carrinho

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.h1`
+   display: flex;
+   justify-content: space-around;
+`
+
+export function Header() {
+    return <Container>
+       
+       <span> Lojinha AstroDev </span>
+       
+    </Container>
+ }
