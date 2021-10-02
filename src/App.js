06f39673867f80136b6fb7331a585d483f521bd0
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-10-02 00:41:50 -0300

    Criado todos os filtros

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import produtosLista from "./produtos/produtos.json"
 import { Card } from './Componentes/Card';
 import React, {Component} from 'react'
 import { Header } from "./Componentes/Header";
+import { Filters } from "./Componentes/Filtros";
 
 const ListContainer = styled.div`
    display: flex;
@@ -12,26 +13,73 @@ const ListContainer = styled.div`
 
 class App extends Component {
   state = {
-    produtos: produtosLista, 
+    produtos: produtosLista,
+    query: "",
+    precoMinimo: "",
+    precoMaximo: "",
+    order: 1 
   }
+
+  
+  updateQuery = (ev) => {
+    this.setState({
+       query: ev.target.value
+    })
+ }
+
+ updatePrecoMinimo = (ev) => {
+    this.setState({
+       precoMinimo: ev.target.value
+    })
+ }
+
+ updatePrecoMaximo = (ev) => {
+    this.setState({
+       precoMaximo: ev.target.value
+    })
+ }
+
+updateOrder = (ev) => {
+    this.setState({
+       order: ev.target.value
+    })
+ }
+
 
   render(){
 
   return <>
    <Header />
 
-
-
-   <ListContainer>
-        {this.state.produtos.map(produtos => {
-               return <Card key={produtos.id} produtos={produtos} />
-            })}
+   <Filters
+      query={this.state.query}
+      updateQuery={this.updateQuery}
+      updatePrecoMinimo={this.updatePrecoMinimo}
+      updatePrecoMaximo={this.updatePrecoMaximo}
+      updateOrder={this.updateOrder}
+      precoMinimo={this.state.precoMinimo}
+      precoMaximo={this.state.precoMaximo}
+      order={this.state.order}
+    />
        
-  </ListContainer>
+   <ListContainer>
+      {this.state.produtos
+      .filter(produtos  => {
+      return produtos.nome.toLowerCase().includes(this.state.query.toLowerCase())
+      })
+      .filter(produtos => {
+      return this.state.precoMinimo === "" || produtos.valor >= this.state.precoMinimo
+      })
+      .filter(produtos => {
+      return this.state.precoMaximo === "" || produtos.valor <= this.state.precoMaximo
+      })
+      .map(produtos => {
+      return <Card key={produtos.id} produtos={produtos} />
+      })}
+ </ListContainer>
   
 </>
   }
 }
 
 export default App;
-
